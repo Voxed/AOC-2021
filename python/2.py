dbf7d05fb8426e9187bdd9d54a16259494b868fc
Voxed <voxedg@gmail.com>
2021-12-03 12:13:33 +0100

    Is this code golf yet?

--- python/2.py
+++ python/2.py
@@ -1,19 +1,15 @@
 import sys
 
-def process_commands(commands, depth_strat = 'simple'):
-    aim = horizontal = depth = 0
-    for (command, magnitude) in commands:
-        magnitude = int(magnitude)
-        if command == 'forward':
-            horizontal += magnitude
-            depth += aim*magnitude if depth_strat == 'aim' else 0
-        else:
-            magnitude = magnitude * (-1 if command == 'up' else 1)
-            aim += magnitude
-            depth += magnitude if depth_strat == 'simple' else 0
-    return depth*horizontal
+def process_commands(cmds, depth_strat = 'simple'):
+    d = h = a = 0
+    for (c, m) in cmds:
+        (fc, uc, dc) = [int(t == c) for t in ['forward', 'up', 'down']]
+        h += m*fc
+        a += d*m*fc
+        d += m * (-uc + dc) 
+    return (d if depth_strat == 'simple' else a)*h
 
 with open(sys.argv[1], 'r') as f:
-    commands = [cmd.split(' ') for cmd in f.readlines()]
-    print(f"""Part 1: {process_commands(commands, depth_strat='simple')}
-Part 2: {process_commands(commands, depth_strat='aim')}""")
+    cmds = [(c, int(m)) for l in f.readlines() for (c, m) in [l.split(' ')]]
+    print(f"""Part 1: {process_commands(cmds, depth_strat='simple')}
+Part 2: {process_commands(cmds, depth_strat='aim')}""")
