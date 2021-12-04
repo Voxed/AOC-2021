e4e1a333678a8aebce5db5ce60ffd0f282a7b88c
Petter Blomkvist <voxedg@gmail.com>
2021-12-04 10:50:50 +0100

    Some improvements that were pointed out to me :)

--- python/4.py
+++ python/4.py
@@ -3,16 +3,16 @@ import numpy as np
 
 with open(sys.argv[1], 'r') as f:    
     sequence = list(map(lambda x: int(x), f.readline().split(',')))            
-    boards = np.concatenate([list(map(lambda x: int(x), filter(lambda x: x != '', line.strip().split(' ')))) for line in f.readlines()]).ravel()
-    boards = [np.array(boards[x:x+5*5]).reshape(5,5) for x in range(0, len(boards), 5*5)]
+    boards = np.concatenate([np.array(line.strip().split(), dtype=int) for line in f.readlines()]).ravel().reshape(-1,5,5)
     last_winner = None
     for s in sequence:
-        boards = [b*(-2*(b == s)+1) for b in boards if b is not None]
+        boards = [np.where(b != s, b, -1) for b in boards if b is not None]
         for (i, b) in enumerate(boards):
-            if np.any([np.all((b < 0)[:, x]) or np.all((b < 0)[x]) for x in range(0, 5)]):
+            if (b == -1).all(axis=0).any() or (b == -1).all(axis=1).any():
                 final_num = s
                 if last_winner is None:
-                    print(f"""Part 1: {b[b >= 0].sum()*final_num}""")
+                    print(b)
+                    print(f"""Part 1: {b[b != -1].sum()*final_num}""")
                 last_winner = b
                 boards[i] = None
-    print(f"""Part 2: {last_winner[last_winner >= 0].sum()*final_num}""")
+    print(f"""Part 2: {last_winner[last_winner != -1].sum()*final_num}""")
