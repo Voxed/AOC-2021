c883535a38bfd82769a270ee62c865fed72c525c
Voxed <voxedg@gmail.com>
2021-12-08 18:32:44 +0100

    Hehe coolio

--- python/8.py
+++ python/8.py
@@ -1,55 +1,27 @@
 import sys
 import numpy as np
 
-def deduce(segments, deduced_segments):
-    if segments.shape[0] == 5:
-        if np.intersect1d(segments, deduced_segments[1]).shape[0] == 2:
-            return 3
-        elif np.intersect1d(segments, deduced_segments[4]).shape[0] == 2:
-            return 2
-        else:
-            return 5
-    elif segments.shape[0] == 6:
-        if np.intersect1d(segments, deduced_segments[7]).shape[0] != 3:
-            return 6
-        elif np.intersect1d(segments, deduced_segments[4]).shape[0] == 3:
-            return 0
-        else:
-            return 9
-    elif segments.shape[0] == 2:
-        return 1
-    elif segments.shape[0] == 4:
-        return 4
-    elif segments.shape[0] == 3:
-        return 7
-    elif segments.shape[0] == 7:
-        return 8
-    return -1 # Can't deduce yet.
+segment_features = np.array([
+    [6, 2, 3, 3], [2, 2, 2, 2], [5, 1, 2, 2], [5, 2, 3, 3], [4, 2, 4, 2],
+    [5, 1, 3, 2], [6, 1, 3, 2], [3, 2, 2, 3], [7, 2, 4, 3], [6, 2, 4, 3],
+])
 
-with open(sys.argv[1], 'r') as f:    
-    inputs = np.array([
-        [np.char.array(list(s)) for s in i.split()] 
-        for l in f.readlines() for i in l.split('|')], dtype=object).reshape(-1, 2)
+def deduce(segment, deductions):
+    similarities = (deductions & segment).sum(axis=1)
+    feature = np.concatenate(([segment.sum()], similarities[[1,4,7]]))
+    return np.argmax((feature == segment_features).sum(axis=1))
 
+with open(sys.argv[1], 'r') as f:
+    segments = [[[np.where(np.in1d(np.arange(7), (np.array([ord(c) for c in s]) - 97)), 1, 0) for s in seq.split()] for seq in l.split('|')] for l in f.readlines()]
     count = 0
-    first_count = 0
-    for (train, test) in inputs:
-        deduced_segments = [np.array([])] * 10
-        while len([s for s in deduced_segments if len(s) == 0]) > 0:
-            for seg in train:
-                n = deduce(seg, deduced_segments)
-                if n >= 0:
-                    deduced_segments[deduce(seg, deduced_segments)] = seg
-
-        number_string = ""
-        for seg in test:
-            for value, deducded_seg in enumerate(deduced_segments):
-                if np.intersect1d(seg, deducded_seg).shape[0] == seg.shape[0] == deducded_seg.shape[0]:
-                    if value in [1, 4, 7, 8]:
-                        first_count += 1
-                    number_string += str(value)
-
-        count += int(number_string)
-
-    print(first_count)
+    count_nums = 0
+    for (train, output) in segments:
+        segment_deductions = np.full((10,7), -1)
+        while((segment_deductions == -1).sum() > 0):
+            for segment in train:
+                segment_deductions[deduce(segment, segment_deductions)] = segment
+        deduced_ouput = np.array([deduce(seg, segment_deductions) for seg in output])
+        count_nums += np.in1d(deduced_ouput, [1,4,7,8]).sum()
+        count += int(''.join([str(num) for num in deduced_ouput]))
+    print(count_nums)
     print(count)
